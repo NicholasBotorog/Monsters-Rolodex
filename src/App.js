f9b5e837ee678b43549c2962fa9a8f9d4cf35a19
Nicholas <razvan.botorog@yahoo.com>
2022-12-15 17:29:40 -0500

    input search function

--- src/App.js
+++ src/App.js
@@ -25,27 +25,27 @@ class App extends Component {
       }))
   }
 
-
+  onSearchChange = (event)=>{
+      const searchField = event.target.value.toLowerCase()
+      this.setState(() => { 
+        return { searchField }
+      })
+    }
+  
 
   render() {
 
+    const { monsters, searchField } = this.state
+    const { onSearchChange } = this
+
     const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(this.state.searchField)
+      return monster.name.toLowerCase().includes(searchField)
     })
 
     return (
       <div className="App">
 
-        <input
-          className='searchBox'
-          type='search'
-          placeholder='Search Monsters...' 
-          onChange={(event)=>{
-          const searchField = event.target.value.toLowerCase()
-          this.setState(() => { 
-            return { searchField }
-          })
-        }}/>
+        <input className='searchBox' type='search' placeholder='Search Monsters...'  onChange={onSearchChange}/>
 
         {filteredMonsters.map(monster => { 
             return ( 
